c6e3fface4603f4b47d2b7029e7169dd639a72ce
Sena-Sakalli <senassakalli@gmail.com>
2021-02-15 03:46:26 +0300

    15.02.2021

--- ReCapProject/Business/Concrete/CarManager.cs
+++ ReCapProject/Business/Concrete/CarManager.cs
@@ -19,5 +19,32 @@ namespace Business.Concrete
         {
             return _carDal.GetAll();
         }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c=>c.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
+        }
+        public List<Car> GetCarsDailyPrice(decimal min, decimal max)
+        {
+            return _carDal.GetAll(c => c.DailyPrice >= min && c.DailyPrice <= max);
+        }
+        public void Add(Car car) 
+        {
+            if (car.Description.Length >= 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+            }
+            else
+            {
+                Console.WriteLine("Car description must contain at least 2 characters and DailyPrice > 0");
+            }
+        }
+
+        
     }
 }
--- ReCapProject/DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ ReCapProject/DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 
@@ -52,6 +53,14 @@ namespace DataAccess.Concrete.InMemory
             return _cars.Where(c=>c.Id==carId).ToList();
         }
 
-      
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
